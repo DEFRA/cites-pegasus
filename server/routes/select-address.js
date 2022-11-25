71dc7ac0ffbf70ffdd414cc055467de25e0ffd0d
Ben Foster <ben.foster@capgemini.com>
2022-11-25 12:15:00 +0000

    Bug fix for back button on select address

--- server/routes/select-address.js
+++ server/routes/select-address.js
@@ -139,7 +139,11 @@ module.exports = [{
             return h.redirect(`${invalidAppDataPath}/`)
         }
 
-        const pageData = { partyType: request.params.partyType, permitType: appData?.permitType, isAgent: appData?.isAgent, ...appData[request.params.partyType].addressSearchData }
+        const pageData = { 
+            partyType: request.params.partyType, 
+            permitType: appData?.permitType, 
+            isAgent: appData?.isAgent, 
+            ...appData[request.params.partyType]?.addressSearchData }
         return h.view(pageId, createModel(null, pageData));
 
     }
@@ -162,6 +166,7 @@ module.exports = [{
                     partyType: request.params.partyType, 
                     permitType: appData?.permitType, 
                     isAgent: appData?.isAgent, 
+                    ...appData[request.params.partyType]?.addressSearchData,
                     ...request.payload 
                 }
 
