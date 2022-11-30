4db979d039ea255e7d96d50d23aee5b2419ed8d2
ybayrakt <ybayrakt@adcs-MBP.broadband>
2022-11-30 16:42:56 +0000

    class added for radios

--- server/routes/purpose-code.js
+++ server/routes/purpose-code.js
@@ -47,6 +47,7 @@ function createModel(errors, purposeCode) {
     pageTitle: errorList
       ? commonContent.errorSummaryTitlePrefix + errorList[0].text
       : pageContent.defaultTitle,
+
     inputPurposeCode: {
       idPrefix: "purposeCode",
       name: "purposeCode",
@@ -62,74 +63,108 @@ function createModel(errors, purposeCode) {
           value: "B",
           text: pageContent.radioOptionB,
           hint: { text: pageContent.radioOptionBHint },
-          classes:
-            "govuk-body govuk-!-font-weight-bold govuk-!-margin-bottom-0",
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "B")
         },
         {
           value: "E",
           text: pageContent.radioOptionE,
           hint: { text: pageContent.radioOptionEHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "E")
         },
         {
           value: "G",
           text: pageContent.radioOptionG,
           hint: { text: pageContent.radioOptionGHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "G")
         },
         {
           value: "H",
           text: pageContent.radioOptionH,
           hint: { text: pageContent.radioOptionHHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "H")
         },
         {
           value: "L",
           text: pageContent.radioOptionL,
           hint: { text: pageContent.radioOptionLHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "L")
         },
         {
           value: "M",
           text: pageContent.radioOptionM,
           hint: { text: pageContent.radioOptionMHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "M")
         },
         {
           value: "N",
           text: pageContent.radioOptionN,
           hint: { text: pageContent.radioOptionNHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "N")
         },
         {
           value: "P",
           text: pageContent.radioOptionP,
           hint: { text: pageContent.radioOptionPHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "P")
         },
         {
           value: "Q",
           text: pageContent.radioOptionQ,
           hint: { text: pageContent.radioOptionQHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "Q")
         },
         {
           value: "S",
           text: pageContent.radioOptionS,
           hint: { text: pageContent.radioOptionSHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "S")
         },
         {
           value: "T",
           text: pageContent.radioOptionT,
           hint: { text: pageContent.radioOptionTHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "T")
         },
         {
           value: "Z",
           text: pageContent.radioOptionZ,
           hint: { text: pageContent.radioOptionZHint },
+          label: {
+            classes: "govuk-!-font-weight-bold"
+          },
           checked: isChecked(purposeCode, "Z")
         }
       ],
@@ -146,7 +181,8 @@ module.exports = [
     handler: async (request, h) => {
       const appData = getAppData(request)
       // validateAppData(appData, pageId)
-      return h.view(pageId, createModel(null, appData?.purposeCode))
+      
+      return h.view(pageId, createModel(null, appData?.species.purposeCode, appData?.species.speciesName, appData?.species.quantity))
     }
   },
   {
