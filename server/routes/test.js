9b509d41eab4167c4108b9b60ebdc2f875c659ef
Ben Foster <ben.foster@capgemini.com>
2022-12-07 17:06:53 +0000

    Config test

--- server/routes/test.js
+++ server/routes/test.js
@@ -2,50 +2,16 @@ const urlPrefix = require('../../config/config').urlPrefix
 const pageId = 'test'
 const Joi = require('joi')
 const { whoAmI } = require('../services/dynamics-service')
+const config = require('../../config/config')
 
 module.exports = [
   {
     method: 'GET',
     path: `${urlPrefix}/test`,
     handler: async (request, h) => {
-      //const authToken = await auth.getClientCredentialsToken()
-
-      // const searchData = {
-      //   //postcode: null,
-
-      //   property: null,
-      //   street: 'dfgfdf',
-      //   town: 'x'
-      // }
-
-      // if(searchData.postcode && (searchData.property || searchData.street || searchData.town)){
-      //   console.log('error both')
-      // } else if(!searchData.postcode && !searchData.property && !searchData.street && !searchData.town){
-      //   console.log('error neither')
-      // } else {
-      //   console.log('all good')
-      // }
-
-
-      // const schema = Joi.object({
-      //   postcode: Joi.string(),
-      //   property: Joi.string()
-      //   // street: Joi.string().empty(),
-      //   // town: Joi.string().empty()
-      // }).or('property', 'postcode')
-
-
-      // // Validate config
-      // const { error, value } = schema.validate({postcode: 'abc', property: ''})
-
-      // // Throw if config is invalid
-      // if (error) {
-      //   console.log(error)
-      // } else {
-      //   console.log('All good')
-      // }
-
-      return h.view(pageId, null);
+      
+      
+      return h.view(pageId, { anothertestsetting: config.ANOTHERTESTSETTING });
     }
   },
   {
