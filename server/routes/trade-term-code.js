4eebeeacfe629ec51b873c633a4ed47e52ba233f
ybayrakt <ybayrakt@adcs-MBP.broadband>
2023-01-18 08:53:07 +0000

    refactored the source code page

--- server/routes/trade-term-code.js
+++ server/routes/trade-term-code.js
@@ -197,21 +197,14 @@ module.exports = [
       handler: async (request, h) => {
         const appData = getAppData(request)
 
-        const specimen =
-          appData.species[request.params.speciesIndex].specimens[
-            request.params.specimenIndex
-          ]
+        const specimen = appData.species[request.params.speciesIndex].specimens[request.params.specimenIndex]
 
         if (!request.payload.isTradeTermCode) {
           specimen.tradeTermCode = ""
         }
 
-        const tradeTermCode = request.payload.isTradeTermCode
-          ? request.payload.tradeTermCode.toUpperCase()
-          : ""
-
         specimen.isTradeTermCode = request.payload.isTradeTermCode
-        specimen.tradeTermCode = tradeTermCode
+        specimen.tradeTermCode = request.payload.isTradeTermCode ? request.payload.tradeTermCode.toUpperCase() : ""
 
         try {
           mergeAppData(
