24be1c51b7e95b530769d4a4578b80691e272499
Ben Foster <ben.foster@capgemini.com>
2023-03-14 16:13:55 +0000

    tidied up changes to oidc.js

--- server/routes/oidc.js
+++ server/routes/oidc.js
@@ -2,26 +2,7 @@ const urlPrefix = require('../../config/config').urlPrefix
 const pageId = 'oidc'
 const openid = require('openid-client');
 //const Joi = require('joi')
-const { getOpenIdClient, oidcClient } = require('../services/oidc-client')
-
-// let client
-
-// (async function createOpenIdClient() {
-  
-//   openid.custom.setHttpOptionsDefaults({
-//     timeout: 10000,
-//   });
-//   const issuer = await openid.Issuer.discover('https://condev5.azure.defra.cloud/idphub/b2c/b2c_1a_signupsignin/.well-known/openid-configuration');
-//   const clientCredentials = {
-//     client_id: 'f566829d-3826-4ec7-9af9-e1229c5f6c25',
-//     client_secret: 'bVQ8Q~8tDWwLk4sP6FPWmNnXQn4C6NTgjgH3fda7',
-//     redirect_uris: ['https://wa-cites-application.azurewebsites.net/oidc'],
-//   };
-  
-//   client = new issuer.Client(clientCredentials);
-  
-//   //return client
-// })()
+const { getOpenIdClient } = require('../services/oidc-client')
 
 module.exports = [
   // {
@@ -61,13 +42,6 @@ module.exports = [
       const oidcClient = await getOpenIdClient()      
       const authorizationUri = oidcClient.authorizationUrl(authOptions)
 
-
-      //await createOpenIdClient()
-      //const authorizationUri = await getAuthorizationUri();
-      //let client = await createOpenIdClient()
-      
-      //const authorizationUri = await client.authorizationUrl(authOptions);
-    
       return h.redirect(authorizationUri);
     },
   },
