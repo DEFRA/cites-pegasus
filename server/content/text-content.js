e3d5ff7c3936f8d7b29093cca3102d9807a6d200
ybayrakt <ybayrakt@adcs-MacBook-Pro.local>
2023-03-14 12:49:26 +0000

    are you sure page created

--- server/content/text-content.js
+++ server/content/text-content.js
@@ -179,6 +179,17 @@ const sourceCodeSchema = Joi.object({
   })
 })
 
+const areYouSureSchema = Joi.object({
+  defaultTitle: Joi.string().required(),
+  pageHeader: Joi.string().required(),
+  pageBody2: Joi.string().required(),
+  pageBody2: Joi.string().allow("", null),
+  errorMessages: Joi.object({
+    "error.areYouSure.any.required": Joi.string().required(),
+  }).required()
+}).required()
+
+
 const schema = Joi.object().keys({
   common: Joi.object({
     serviceName: Joi.string().required(),
@@ -836,7 +847,17 @@ const schema = Joi.object().keys({
     rowTextSpecimenTypePlantLiving: Joi.string().required(),
     rowTextSpecimenTypePlantProduct: Joi.string().required(),
     rowTextSpecimenTypePlantWorked: Joi.string().required(),
-  }).required()
+  }).required(),
+  areYouSure: Joi.object({
+    permitType: areYouSureSchema,
+    scientificName: areYouSureSchema,
+    yourContactDetails: areYouSureSchema,
+    deliveryAddress: areYouSureSchema,
+    importerContactDetails: areYouSureSchema,
+    exporterContactDetails: areYouSureSchema,
+    reexporterContactDetails: areYouSureSchema,
+    article10ContactDetails: areYouSureSchema
+  }).required(),
 })
 
 // Validate config
---
+++ server/routes/are-you-sure.js
@@ -0,0 +1,180 @@
+const Joi = require("joi")
+const urlPrefix = require("../../config/config").urlPrefix
+const { findErrorList, getFieldError } = require("../lib/helper-functions")
+const { getSubmission, mergeSubmission, validateSubmission } = require("../lib/submission")
+const textContent = require("../content/text-content")
+const pageId = "are-you-sure"
+const currentPath = `${urlPrefix}/${pageId}`
+const previousPath = `${urlPrefix}/already-have-a10`
+const nextPathPermitDetails = `${urlPrefix}/permit-details`
+const nextPathComments = `${urlPrefix}/comments`
+const invalidSubmissionPath = urlPrefix
+
+function createModel(errors, data) {
+  const commonContent = textContent.common
+ 
+  let pageContent = null
+  if(data.contactType === 'applicant'){
+    if(data.isAgent){
+        pageContent = textContent.contactDetails.agentLed
+    } else {
+        pageContent = textContent.contactDetails.applicant
+    }
+} else {
+    pageContent = textContent.contactDetails.agent
+}
+
+
+  let errorList = null
+  if (errors) {
+    errorList = []
+    const mergedErrorMessages = {
+      ...commonContent.errorMessages,
+      ...pageContent.errorMessages
+    }
+    const fields = ["areYouSure"]
+    fields.forEach((field) => {
+      const fieldError = findErrorList(errors, [field], mergedErrorMessages)[0]
+      if (fieldError) {
+        errorList.push({
+          text: fieldError,
+          href: `#${field}`
+        })
+      }
+    })
+  }
+
+  const model = {
+    backLink: `${previousPath}/${data.applicationIndex}`,
+    formActionPage: `${currentPath}/${data.applicationIndex}`,
+    ...(errorList ? { errorList } : {}),
+    pageTitle: errorList
+      ? commonContent.errorSummaryTitlePrefix + errorList[0].text
+      : pageContent.defaultTitle,
+    captionText: data.speciesName,
+
+    inputAreYouSure: {
+      idPrefix: "areYouSure",
+      name: "areYouSure",
+      classes: "govuk-radios--inline",
+      fieldset: {
+        legend: {
+          text: pageContent.pageHeader,
+          isPageHeading: true,
+          classes: "govuk-fieldset__legend--l"
+        }
+      },
+      items: [
+        {
+          value: true,
+          text: commonContent.radioOptionYes,
+          checked: data.isEverImportedExported
+        },
+        {
+          value: false,
+          text: commonContent.radioOptionNo,
+          checked: data.isEverImportedExported === false
+        }
+      ],
+      errorMessage: getFieldError(errorList, "#isEverImportedExported")
+    }
+  }
+  return { ...commonContent, ...model }
+}
+
+module.exports = [
+  {
+    method: "GET",
+    path: `${currentPath}/{applicationIndex}`,
+    options: {
+      validate: {
+        params: Joi.object({
+          applicationIndex: Joi.number().required()
+        })
+      }
+    },
+    handler: async (request, h) => {
+      const { applicationIndex } = request.params
+      const submission = getSubmission(request)
+
+      try {
+        validateSubmission(submission, `${pageId}/${applicationIndex}`)
+      } catch (err) {
+        console.log(err)
+        return h.redirect(`${invalidSubmissionPath}/`)
+      }
+
+      const species = submission.applications[applicationIndex].species
+
+      const pageData = {
+        applicationIndex: applicationIndex,
+        speciesName: species?.speciesName,
+        isEverImportedExported: species.isEverImportedExported
+      }
+      return h.view(pageId, createModel(null, pageData))
+    }
+  },
+  {
+    method: "POST",
+    path: `${currentPath}/{applicationIndex}`,
+    options: {
+      validate: {
+        params: Joi.object({
+          applicationIndex: Joi.number().required()
+        }),
+        options: { abortEarly: false },
+        payload: Joi.object({
+          isEverImportedExported: Joi.boolean().required()
+        }),
+
+        failAction: (request, h, err) => {
+          const { applicationIndex } = request.params
+          const submission = getSubmission(request)
+          const species = submission.applications[applicationIndex].species
+
+          let isEverImportedExported = null
+          switch (request.payload.isEverImportedExported) {
+            case "true":
+              isEverImportedExported = true
+              break
+            case "false":
+              isEverImportedExported = false
+              break
+          }
+
+          const pageData = {
+            applicationIndex: applicationIndex,
+            speciesName: species?.speciesName,
+            isEverImportedExported: isEverImportedExported
+          }
+
+          return h.view(pageId, createModel(err, pageData)).takeover()
+        }
+      },
+      handler: async (request, h) => {
+        const { applicationIndex } = request.params
+        const submission = getSubmission(request)
+        const species = submission.applications[applicationIndex].species
+
+        species.isEverImportedExported = request.payload.isEverImportedExported
+
+        try {
+          mergeSubmission(
+            request,
+            { applications: submission.applications },
+            `${pageId}/${applicationIndex}`
+          )
+        } catch (err) {
+          console.log(err)
+          return h.redirect(`${invalidSubmissionPath}/`)
+        }
+
+        if (request.payload.isEverImportedExported && submission.permitType !== 'export') {
+          return h.redirect(`${nextPathPermitDetails}/${applicationIndex}`)
+        } else {
+          return h.redirect(`${nextPathComments}/${applicationIndex}`)
+        }
+      }
+    }
+  }
+]
