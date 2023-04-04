cb84c0e9e75c40a37f15a419e232adac1ab6b033
Ben Foster <ben.foster@capgemini.com>
2023-04-04 15:17:59 +0100

    Added to the GetSubmissions stub

--- server/services/dynamics-service.js
+++ server/services/dynamics-service.js
@@ -6,7 +6,7 @@ var moment = require('moment');
 const config = require('../../config/config').dynamicsAPI
 const { readSecret } = require('../lib/key-vault')
 
-async function  getClientCredentialsToken() {
+async function getClientCredentialsToken() {
   const clientId = await readSecret('DYNAMICS-API-CLIENT-ID')
   const clientSecret = await readSecret('DYNAMICS-API-CLIENT-SECRET')
 
@@ -70,8 +70,8 @@ async function getSpecies(request, speciesName) {
   try {
     const url = `${config.baseURL}cites_species(cites_scientificname='${speciesName.trim()}')`
     const { res, payload } = await Wreck.get(url, { json: true, headers: { 'Authorization': `Bearer ${accessToken}` } })
-    
-    if(payload.cites_species_response){
+
+    if (payload.cites_species_response) {
       const json = payload.cites_species_response.replace(/(\r\n|\n|\r)/gm, "")
       return JSON.parse(json)
     }
@@ -84,12 +84,35 @@ async function getSpecies(request, speciesName) {
 }
 
 //Stubs
-async function getSubmissions(request, contactId, permitTypes, statuses, startIndex, pageSize) {
-  return [
-    { submissionId: 'AB1234', status: 'Received', dateSubmitted: '2023-04-02T14:02:40.000Z', permitType: 'import' }, 
-    { submissionId: 'CD5678', status: 'Awaiting payment', dateSubmitted: '2023-04-01T09:35:12.000Z', permitType: 'export' },
-    { submissionId: 'EF9012', status: 'Issued', dateSubmitted: '2023-03-28T22:59:59.000Z', permitType: 'article10'  }
+async function getSubmissions(request, contactId, permitTypes, statuses, startIndex, pageSize, searchTerm) {
+  const submissions = [
+    { submissionId: 'AB1234', contactId: '9165f3c0-dcc3-ed11-83ff-000d3aa9f90e', status: 'received', dateSubmitted: '2023-04-02T14:02:40.000Z', permitType: 'import' },
+    { submissionId: 'CD5678', contactId: '9165f3c0-dcc3-ed11-83ff-000d3aa9f90e', status: 'awaiting payment', dateSubmitted: '2023-04-01T09:35:12.000Z', permitType: 'export' },
+    { submissionId: 'EF9012', contactId: '9165f3c0-dcc3-ed11-83ff-000d3aa9f90e', status: 'issued', dateSubmitted: '2023-03-28T22:59:59.000Z', permitType: 'import' },
+    { submissionId: 'GH1212', contactId: '9165f3c0-dcc3-ed11-83ff-000d3aa9f90e', status: 'issued', dateSubmitted: '2023-03-27T22:59:59.000Z', permitType: 'article10' },
+    { submissionId: 'IJ2323', contactId: '9165f3c0-dcc3-ed11-83ff-000d3aa9f90e', status: 'issued', dateSubmitted: '2023-03-25T22:59:59.000Z', permitType: 'import' },
+    { submissionId: 'KL4545', contactId: '9165f3c0-dcc3-ed11-83ff-000d3aa9f90e', status: 'issued', dateSubmitted: '2023-03-01T22:59:59.000Z', permitType: 'reexport' },
+    { submissionId: 'MN5656', contactId: '9165f3c0-dcc3-ed11-83ff-000d3aa9f90e', status: 'issued', dateSubmitted: '2022-02-28T22:59:59.000Z', permitType: 'article10' }
   ]
+  
+  let filteredSubmissions = submissions.filter(submission => {
+    if (contactId && submission.contactId !== contactId) {
+      return false
+    }
+    if (permitTypes && !permitTypes.includes(submission.permitType)) {
+      return false
+    }
+    if (statuses && !statuses.includes(submission.status)) {
+      return false
+    }
+    return true
+  })
+
+  const endIndex = startIndex + pageSize;
+  filteredSubmissions = filteredSubmissions.slice(startIndex, endIndex);
+
+  return filteredSubmissions;
+
 }
 
 module.exports = { whoAmI, getSpecies, getSubmissions }
