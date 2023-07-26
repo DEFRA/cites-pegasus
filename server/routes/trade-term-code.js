ef8f74d43f29d4fcce6e715c6e5db220b58bb974
Ben Foster <ben.foster@capgemini.com>
2023-07-26 11:48:57 +0100

    Removed radio buttons from Trade Term Code page

--- server/routes/trade-term-code.js
+++ server/routes/trade-term-code.js
@@ -12,6 +12,7 @@ const previousPathQuantity = `${urlPrefix}/quantity`
 const previousPathCreatedDate = `${urlPrefix}/created-date`
 const nextPath = `${urlPrefix}/unique-identification-mark`
 const invalidSubmissionPath = `${urlPrefix}/`
+const unknownTradeTermCodeValue = 'UKN'
 
 function createModel(errors, data) {
   const commonContent = textContent.common
@@ -24,7 +25,7 @@ function createModel(errors, data) {
       ...commonContent.errorMessages,
       ...pageContent.errorMessages
     }
-    const fields = ["isTradeTermCode", "tradeTermCode"]
+    const fields = ["tradeTermCode"]
     fields.forEach((field) => {
       const fieldError = findErrorList(errors, [field], mergedErrorMessages)[0]
       if (fieldError) {
@@ -36,11 +37,19 @@ function createModel(errors, data) {
     })
   }
 
-  const tradeTermCodes = [{
+  const tradeTermCodes = []
+
+  tradeTermCodes.push({
     text: pageContent.tradeTermCodeSelectDefault,
     value: '',
     selected: false
-  }]
+  })
+
+  tradeTermCodes.push({
+    text: pageContent.tradeTermCodeUnknown,
+    value: unknownTradeTermCodeValue,
+    selected: data.isTradeTermCode === false
+  })  
 
   tradeTermCodes.push(...data.tradeTermCodes.map(tradeTermCode => {
     return {
@@ -63,21 +72,20 @@ function createModel(errors, data) {
 
   nunjucks.configure(['node_modules/govuk-frontend/'], { autoescape: true, watch: false })
 
-  const tradeTermCodeSelect = nunjucks.renderString(renderString, {
-    selectTradeTermCode: {
-      id: "tradeTermCode",
-      name: "tradeTermCode",
-      classes: "govuk-!-width-2",
-      // label: {
-      //   text: pageContent.inputLabelTradeCode
-      // },
-      // hint: {
-      //   text: pageContent.inputLabelTradeCodeHint
-      // },
-      items: tradeTermCodes,
-      errorMessage: getFieldError(errorList, '#tradeTermCode')
-    }
-  })
+  const selectTradeTermCode = {
+    id: "tradeTermCode",
+    name: "tradeTermCode",
+    classes: "govuk-!-width-2",
+    // label: {
+    //   text: pageContent.inputLabelTradeCode
+    // },
+    // hint: {
+    //   text: pageContent.inputLabelTradeCodeHint
+    // },
+    items: tradeTermCodes,
+    errorMessage: getFieldError(errorList, '#tradeTermCode')
+  }
+
 
   const defaultBacklink = data.createdDate ? `${previousPathCreatedDate}/${data.applicationIndex}` : `${previousPathQuantity}/${data.applicationIndex}`
   const backLink = data.backLinkOverride ? data.backLinkOverride : defaultBacklink
@@ -92,33 +100,34 @@ function createModel(errors, data) {
     captionText: data.speciesName,
     pageHeader: pageContent.pageHeader,
     pageBody: pageContent.pageBody,
-    inputIsTradeTermCode: {
-      idPrefix: "isTradeTermCode",
-      name: "isTradeTermCode",
-      // fieldset: {
-      //   legend: {
-      //     text: pageContent.pageHeader,
-      //     isPageHeading: true,
-      //     classes: "govuk-fieldset__legend--l"
-      //   }
-      // },
-      items: [
-        {
-          value: true,
-          text: commonContent.radioOptionYes,
-          checked: data.isTradeTermCode,
-          conditional: {
-            html: tradeTermCodeSelect
-          }
-        },
-        {
-          value: false,
-          text: pageContent.radioOptionNotSure,
-          checked: data.isTradeTermCode === false
-        }
-      ],
-      errorMessage: getFieldError(errorList, "#isTradeTermCode")
-    }
+    selectTradeTermCode
+    // inputIsTradeTermCode: {
+    //   idPrefix: "isTradeTermCode",
+    //   name: "isTradeTermCode",
+    //   // fieldset: {
+    //   //   legend: {
+    //   //     text: pageContent.pageHeader,
+    //   //     isPageHeading: true,
+    //   //     classes: "govuk-fieldset__legend--l"
+    //   //   }
+    //   // },
+    //   items: [
+    //     {
+    //       value: true,
+    //       text: commonContent.radioOptionYes,
+    //       checked: data.isTradeTermCode,
+    //       conditional: {
+    //         html: tradeTermCodeSelect
+    //       }
+    //     },
+    //     {
+    //       value: false,
+    //       text: pageContent.radioOptionNotSure,
+    //       checked: data.isTradeTermCode === false
+    //     }
+    //   ],
+    //   errorMessage: getFieldError(errorList, "#isTradeTermCode")
+    // }
   }
   return { ...commonContent, ...model }
 }
@@ -169,33 +178,35 @@ module.exports = [
         }),
         options: { abortEarly: false },
         payload: Joi.object({
-          isTradeTermCode: Joi.boolean().required(),
-          tradeTermCode: Joi.when("isTradeTermCode", {
-            is: true,
-            then: Joi.string().length(3).regex(ALPHA_REGEX).required()
-          })
-        }),
+          tradeTermCode: Joi.string().length(3).regex(ALPHA_REGEX).required()
+        }
+        ),
 
         failAction: (request, h, err) => {
           const { applicationIndex } = request.params
           const submission = getSubmission(request)
           const species = submission.applications[applicationIndex].species
 
+          // let isTradeTermCode = null
+          // switch (request.payload.isTradeTermCode) {
+          //   case "true":
+          //     isTradeTermCode = true
+          //     break
+          //   case "false":
+          //     isTradeTermCode = false
+          //     break
+          // }
+
           let isTradeTermCode = null
-          switch (request.payload.isTradeTermCode) {
-            case "true":
-              isTradeTermCode = true
-              break
-            case "false":
-              isTradeTermCode = false
-              break
+          if(request.payload.tradeTermCode){
+            isTradeTermCode = request.payload.tradeTermCode !== unknownTradeTermCodeValue
           }
 
           const pageData = {
             backLinkOverride: checkChangeRouteExit(request, true),
             applicationIndex: applicationIndex,
             speciesName: species.speciesName,
-            isTradeTermCode: isTradeTermCode,
+            isTradeTermCode, 
             tradeTermCode: request.payload.tradeTermCode,
             createdDate: species.createdDate,
             tradeTermCodes: request.server.app.tradeTermCodes
@@ -208,16 +219,17 @@ module.exports = [
         const { applicationIndex } = request.params
         const submission = getSubmission(request)
         const species = submission.applications[applicationIndex].species
-
-        if (!request.payload.isTradeTermCode) {
-          species.tradeTermCode = ""
-        }
-
         const selectedTradeTermCode = request.server.app.tradeTermCodes.find(tradeTermCode => tradeTermCode.code === request.payload.tradeTermCode)
 
-        species.isTradeTermCode = request.payload.isTradeTermCode
-        species.tradeTermCode = request.payload.isTradeTermCode ? request.payload.tradeTermCode.toUpperCase() : ""
-        species.tradeTermCodeDesc = request.payload.isTradeTermCode ? selectedTradeTermCode.name : ""
+        if (request.payload.tradeTermCode === unknownTradeTermCodeValue) {
+          species.isTradeTermCode = false
+          species.tradeTermCode = ""
+          species.tradeTermCodeDesc = ""
+        } else {               
+          species.isTradeTermCode = true
+          species.tradeTermCode = request.payload.tradeTermCode.toUpperCase()
+          species.tradeTermCodeDesc = selectedTradeTermCode.name
+        }
 
         try {
           mergeSubmission(request, { applications: submission.applications }, `${pageId}/${applicationIndex}`)
