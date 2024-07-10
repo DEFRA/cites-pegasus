c99a5ff6025cccf3d1cef42185a87ecada81c3ae
Ben Foster <ben.foster@capgemini.com>
2024-07-10 14:08:30 +0100

    Enabled memory caching instead of Redis

--- config/cache.js
+++ config/cache.js
@@ -16,7 +16,7 @@ const schema = Joi.object({
 })
 
 const config = {
-  useRedis: true, //process.env.NODE_ENV !== 'test',
+  useRedis: false, //process.env.NODE_ENV !== 'test',
   expiresIn: process.env.SESSION_CACHE_TTL,
   catboxOptions: {
     host: process.env.REDIS_HOSTNAME,
@@ -38,8 +38,12 @@ if (result.error) {
 }
 
 async function getCacheConfig(){    
-  const redisPassword = await readSecret('REDIS-PASSWORD')
-  result.value.catboxOptions.password = redisPassword.value
+  if(result.value.useRedis){
+    result.value.catboxOptions.password = await readSecret('REDIS-PASSWORD')
+  } else {
+    result.value.catboxOptions = {}
+  }
+
   return result.value
 }
 
