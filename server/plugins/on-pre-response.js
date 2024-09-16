15a02c715e1e674f4d4243bffdf6a9a2917311d6
Ben Foster <ben.foster@capgemini.com>
2024-09-16 10:48:47 +0100

    Changed GA code to Nonce Aware version but without CSP header

--- server/plugins/on-pre-response.js
+++ server/plugins/on-pre-response.js
@@ -15,21 +15,20 @@ const onPreResponse = {
             // `connect-src 'self' https://www.googletagmanager.com;`
             
             const nonce = crypto.randomBytes(16).toString('base64')
-            const cspWithNonce =
-            `default-src 'self'; ` +
-            `style-src 'self' 'unsafe-inline'; ` +
-            `script-src 'self' 'nonce-${nonce}'; ` +
-            `img-src 'self' https://www.googletagmanager.com; ` +
-            `connect-src 'self' https://www.googletagmanager.com;`
+            // const cspWithNonce =
+            // `default-src 'self'; ` +
+            // `style-src 'self' 'unsafe-inline'; ` +
+            // `script-src 'self' 'nonce-${nonce}'; ` +
+            // `img-src 'self' https://www.googletagmanager.com; ` +
+            // `connect-src 'self' https://www.googletagmanager.com;`
             
             if (!response.isBoom) {
                 //Access-Control-Allow-Origin
-                //Remove all X-Powered-By headers,
-                //all X-XSS-Protection headers. Best Practice is to have the Referrer-Policy strict-origin-when-cross-origin instead of the current configuration - Low
+                //Remove all X-Powered-By headers, all X-XSS-Protection headers. 
             
                 //response.header('Content-Security-Policy', cspWithNonce)
                 response.header('X-Permitted-Cross-Domain-Policies', 'none')
-                response.header('Referrer-Policy', 'no-referrer-when-downgrade')
+                response.header('Referrer-Policy', 'no-referrer-when-downgrade') //Best Practice is to have the Referrer-Policy strict-origin-when-cross-origin instead of the current configuration - Low
                 response.header('Permissions-Policy', 'geolocation=(), camera=(), microphone=()')
                 if(response.source?.compiled?.settings?.contentType === 'text/html'){ //Adds this variable to all pages so that it can be accessed by the templates
                     response.source.context.cspNonce = nonce
