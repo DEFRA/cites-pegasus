4ae149a2333a7e1f3f6a2994e22474a190683243
Ben Foster <ben.foster@capgemini.com>
2024-09-16 11:28:17 +0100

    Use basic GA tag with CSP header

--- server/plugins/on-pre-response.js
+++ server/plugins/on-pre-response.js
@@ -7,12 +7,12 @@ const onPreResponse = {
         server.ext('onPreResponse', (request, h) => {
             const response = request.response
             
-            // const cspWithoutNonce = 
-            // `default-src 'self'; ` +
-            // `style-src 'self' 'unsafe-inline'; ` +
-            // `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; ` +
-            // `img-src 'self' https://www.googletagmanager.com; ` +
-            // `connect-src 'self' https://www.googletagmanager.com;`
+            const cspWithoutNonce = 
+            `default-src 'self'; ` +
+            `style-src 'self' 'unsafe-inline'; ` +
+            `script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com; ` +
+            `img-src 'self' https://www.googletagmanager.com; ` +
+            `connect-src 'self' https://www.googletagmanager.com;`
             
             const nonce = crypto.randomBytes(16).toString('base64')
             // const cspWithNonce =
@@ -26,7 +26,7 @@ const onPreResponse = {
                 //Access-Control-Allow-Origin
                 //Remove all X-Powered-By headers, all X-XSS-Protection headers. 
             
-                //response.header('Content-Security-Policy', cspWithNonce)
+                response.header('Content-Security-Policy', cspWithoutNonce)
                 response.header('X-Permitted-Cross-Domain-Policies', 'none')
                 response.header('Referrer-Policy', 'no-referrer-when-downgrade') //Best Practice is to have the Referrer-Policy strict-origin-when-cross-origin instead of the current configuration - Low
                 response.header('Permissions-Policy', 'geolocation=(), camera=(), microphone=()')
