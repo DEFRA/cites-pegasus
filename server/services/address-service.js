b38cc327fbcce45920547708a59983e5c2e3fad4
Jeet Jadhav <jeet.d.jadhav@capgemini.com>
2025-02-19 17:22:44 +0530

    Added proper logs for debugging

--- server/services/address-service.js
+++ server/services/address-service.js
@@ -4,25 +4,27 @@ const { readSecret } = require("../lib/key-vault");
 const { getYarValue, setYarValue, sessionKey } = require("../lib/session");
 const _ = require("lodash");
 
+const clientId = await readSecret("CLIENT_ID");
+const clientSecret = await readSecret("CLIENT_SECRET");
+const grantType = await readSecret("GRANT_TYPE");
+const scope = await readSecret("SCOPE");
+const authURL = await readSecret("AUTH_URL");
+
 const getAPIMAccessToken = async (request) => {
-  const clientId = await readSecret("CLIENT_ID");
-  const clientSecret = await readSecret("CLIENT_SECRET");
-  const grantType = await readSecret("GRANT_TYPE");
-  const scope = await readSecret("SCOPE");
-  const authURL = await readSecret("AUTH_URL");
+  const payload = new URLSearchParams();
+  payload.append("client_id", clientId);
+  payload.append("client_secret", clientSecret);
+  payload.append("grant_type", grantType);
+  payload.append("scope", scope);
 
-  const payload = {
-    client_id: clientId,
-    client_secret: clientSecret,
-    grant_type: grantType,
-    scope: scope,
+  const options = {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    payload: payload.toString(),
   };
 
-  const headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-  };
-
-  console.log("AUTH_REQ_OPTIONS Payload", payload);
+  console.log(`[APIM_AUTH_REQ_OPTIONS]: ${payload}`);
 
   try {
     const { payload } = await Wreck.post(authURL, options);
@@ -32,12 +34,12 @@ const getAPIMAccessToken = async (request) => {
       accessToken: tokenResponse.access_token,
       expiryTime: new Date().getTime() + tokenResponse.expires_in * 1000,
     };
-
+    console.log(`[APIM_ACCESS_TOKEN]: ${data}`);
     setYarValue(request, sessionKey.APIM_ACCESS_TOKEN, data);
     console.log("TOKEN_RESPONSE ", data);
     return tokenResponse.access_token;
   } catch (error) {
-    console.error("ERROR_WHILE_GEN_TOKEN ", error);
+    console.log(`[APIM_ERROR_WHILE_GEN_TOKEN]: ${error}`);
     throw error;
   }
 };
@@ -47,21 +49,23 @@ const validateAPIMToken = (token) => {
     const currentTime = new Date().getTime();
 
     if (currentTime < token.expiryTime) {
+      console.log(`[APIM_TOKEN_FOUND]`);
       return true;
     } else {
+      console.log(`[APIM_TOKEN_EXPIRED]`);
       return false;
     }
   } else {
+    console.log(`[APIM_TOKEN_EXPIRED]`);
     return false;
   }
 };
 
 async function getAddressesByPostcode(postcode, request) {
   try {
-    const sessionAPIM = getYarValue(request, sessionKey.APIM_ACCESS_TOKEN);
-
     let token;
-
+    const sessionAPIM = getYarValue(request, sessionKey.APIM_ACCESS_TOKEN);
+    console.log(`[APIM_ACCESS_TOKEN_FROM_COOKIE]: ${sessionAPIM}`);
     if (!validateAPIMToken(sessionAPIM)) {
       token = await getAPIMAccessToken(request);
     } else {
@@ -76,15 +80,17 @@ async function getAddressesByPostcode(postcode, request) {
           Authorization: `Bearer ${token}`,
         },
       });
-
+      console.log(`[APIM_TO_GET_ADDRESS]: ${url}, ${payload}`);
       if (payload && res.statusCode !== httpStatusCode.NO_CONTENT) {
         console.log(JSON.parse(payload));
         return JSON.parse(payload);
       }
+    } else {
+      console.log(`[APIM_TOKEN_NOT_FOUND]: ${authURL}`);
     }
     return { results: [] };
   } catch (err) {
-    console.error("ERROR_WHILE_FETCHING_ADDRESS ", err);
+    console.log(`[ERROR_WHILE_FETCHING_ADDRESS]: ${err}`);
     throw err;
   }
 }
